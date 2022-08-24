9196775536a8945c27a86b0ffc1d122e0b010647
蔡偲 <caicai9514@163.com>
2022-08-24 10:44:13 +0800

    task:select组件初步封装完成

--- src/views/select-demo/SelectDemo.vue
+++ src/views/select-demo/SelectDemo.vue
@@ -1,13 +1,35 @@
 <template>
 	<div class="demo-container">
 		<fx-select
-			placeholder="请输入关键字"
-		></fx-select>
+			ref="selectRef"
+			v-model="selectValue"
+			clearable
+			:selectOptions="selectOptions"
+			@change="onSelectChange"
+		>
+		<template #option="{optionItem}">
+			<span>
+				{{ '风行--' + optionItem.label}}
+			</span>
+		</template>
+		</fx-select>
 	</div>
 </template>
 
 <script setup>
-
+import { ref, reactive } from 'vue'
+const selectValue = ref('')
+const selectRef = ref()
+const selectOptions = reactive(Array.from({length: 20}, (item,index) => {
+	return {
+		id: index + '',
+		label: "选项" + (index + 1),
+		disabled: index === 2 || index === 5
+	}
+}))
+const onSelectChange = (id, item) => {
+	console.log(id, item)
+}
 </script>
 
 <style lang="stylus" scoped>
